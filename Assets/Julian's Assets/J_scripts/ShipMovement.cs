88476d299822ef606ad54f047796355f203a2955
Jsollozo <33267790+Jsollozo@users.noreply.github.com>
2019-02-07 11:19:34 -0800

    Player movement Added
    Added player movement to the ship, locked on the y and z axis

--- Assets/Julian's Assets/J_scripts/ShipMovement.cs
+++ Assets/Julian's Assets/J_scripts/ShipMovement.cs
@@ -5,14 +5,29 @@ using UnityEngine;
 public class ShipMovement : MonoBehaviour
 {
     // Start is called before the first frame update
+ 
+    private Rigidbody2D ship_rigidbody;
+    [SerializeField] protected float speed = 4;
+
+    private void Awake()
+    {
+        ship_rigidbody = this.GetComponent<Rigidbody2D>();
+    }
+
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        float Move_by_what = Input.GetAxisRaw("Horizontal");
+        Vector2 currentVelocity = ship_rigidbody.velocity;
+        ship_rigidbody.velocity = new Vector2(Move_by_what * speed, currentVelocity.y); 
     }
 }
