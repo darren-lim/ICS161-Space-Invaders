14af9a4e16af29d8ba7af7a014a4755609e0b5d0
Jsollozo <33267790+Jsollozo@users.noreply.github.com>
2019-02-10 21:37:11 -0800

    Shooting Added
    Shooting has been added and it uses a dead box to destroy objects if the player misses. There can only be one bullet at screen at one time.

---
+++ Assets/Julian's Assets/J_scripts/BulletLogic.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletLogic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShipLogic.No_Bullet = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        ShipLogic.No_Bullet = true;
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.CompareTag("DeadBox"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+}
---
+++ Assets/Julian's Assets/J_scripts/ShipLogic.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipLogic : MonoBehaviour
+{
+    // Start is called before the first frame update
+ 
+    private Rigidbody2D ship_rigidbody;
+    private Collider2D ship_collider;
+    [SerializeField] protected float player_speed = 4;
+    private float bullet_speed = 6;
+    public GameObject ShipBulletPrefab;
+    public static bool No_Bullet = true;
+
+    private void Awake()
+    {
+        ship_rigidbody = this.GetComponent<Rigidbody2D>();
+        ship_collider = this.GetComponent<Collider2D>();
+    }
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+        if (Input.GetKeyDown(KeyCode.Space) && No_Bullet)
+        {
+            Shoot();
+        }
+    }
+
+    private void Move()
+    {
+        float Move_by_what = Input.GetAxisRaw("Horizontal");
+        Vector2 currentVelocity = ship_rigidbody.velocity;
+        ship_rigidbody.velocity = new Vector2(Move_by_what * player_speed, currentVelocity.y); 
+    }
+
+    private void Shoot()
+    {
+        GameObject newBullet = Instantiate(ShipBulletPrefab, this.transform.position, Quaternion.identity);
+        newBullet.GetComponent<Rigidbody2D>().velocity = new Vector2(0, bullet_speed);
+        Physics2D.IgnoreCollision(ship_collider, newBullet.GetComponent<Collider2D>());
+    }
+
+    //private bool No_Bullets()
+    //{
+        //if (newBullet.activeInHierarchy == false)
+        //{
+            //return true;
+        //}
+        //return false;
+    //}
+}
