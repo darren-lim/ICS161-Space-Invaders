e703bf53792172842bea6e96ff88423fec65446f
Daniel Alcala <dalcala94@gmail.com>
2019-02-12 00:37:53 -0800

    Start Menu Working
    going to attempt the "pause" Extra credit conditions

---
+++ Assets/Daniel's Assets/da_scripts/StartBehaviourScript.cs
@@ -0,0 +1,27 @@
+﻿/*
+Daniel Alcala
+February 05, 2018
+Game Engine Lab
+Project 2
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class StartBehaviourScript : MonoBehaviour
+{
+ 
+    public void StartGame()
+	{
+		SceneManager.LoadScene("MainTestScene");
+	}
+
+	public void QuitGame()
+	{
+        Application.Quit();
+	}
+
+}
