b79e62e44812665f02b919a1abb44549eea8022d
Jsollozo <33267790+Jsollozo@users.noreply.github.com>
2019-02-11 21:14:02 -0800

    small change

--- Assets/Julian's Assets/J_scripts/GameOverScript.cs
+++ Assets/Julian's Assets/J_scripts/GameOverScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelManager : MonoBehaviour
+public class GameOverScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
